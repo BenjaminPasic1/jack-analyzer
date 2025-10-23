4c6609d2fe26f5e72e1cab20106179d64d8329d6
Benjamin Pasic <benjamin.pasic1@gmail.com>
2025-10-23 23:37:12 +0200

    eat and read_line implementation

--- include/parser/jack_parser.h
+++ include/parser/jack_parser.h
@@ -2,13 +2,17 @@
 #define JACK_PARSER_H
 
 #include <stdio.h>
+#define BUFFER_SIZE 256
+#define EXTRACTED_SIZE 64
 
 extern FILE *tokens_xml;
 extern FILE *parsed_xml;
+extern char buffer[BUFFER_SIZE];
 
 typedef enum { DATA, TYPE } ExtractMode;
 
 void parse_tokens(FILE *tokens);
-void eat(char *current_token, char *exptected_token, ExtractMode mode);
+void eat(char *exptected_token, ExtractMode mode);
+char *read_line_and_extract(ExtractMode mode);
 
 #endif
--- src/parser/jack_parser.c
+++ src/parser/jack_parser.c
@@ -1,9 +1,12 @@
+#include "../../include/parser/jack_parser.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 // Declared as global in .h via extern
 FILE *tokens_xml;
 FILE *parsed_xml;
+char buffer[BUFFER_SIZE];
 
 void parse_tokens(FILE *tokens) {
   // Set tokens_xml as global, so no need to pass it everywhere.
@@ -13,14 +16,89 @@ void parse_tokens(FILE *tokens) {
   parsed_xml = fopen("parsed_output.xml", "w");
 
   if (!parsed_xml) {
-    perror("[ERROR]: Unable to create parsed_xml file. EXITING...");
+    perror("[ERROR] -> parse_tokens: Unable to create parsed_xml file. "
+           "EXITING...\n");
     exit(EXIT_FAILURE);
   }
 }
 
-void eat(char *current_token, char *exptected_token) {
-  if (!current_token || !exptected_token) {
-    perror("[ERROR]: ");
+// Eat should work as follows: I should read a line inside of eat()
+// Extract either the type of token or the token itself
+// If it matches the expected_token, then it's okay. Otherwise exit.
+void eat(char *exptected_token, ExtractMode mode) {
+  // Extract either VALUE or TYPE from the token into global BUFFER
+  char *extracted_val = read_line_and_extract(mode);
+
+  // Compare expected and current token
+  if (strcmp(extracted_val, exptected_token) != 0) {
+    perror("[ERROR] -> eat: Expected_token does NOT match current_token. "
+           "EXITING...\n");
+    free(extracted_val);
     exit(EXIT_FAILURE);
+  }
+
+  free(extracted_val);
+}
+
+char *read_line_and_extract(ExtractMode mode) {
+  char *extracted_string = malloc(EXTRACTED_SIZE);
+
+  if (!fgets(buffer, BUFFER_SIZE, tokens_xml)) {
+    perror("[ERROR] -> read_line: Could not read the next line. EXITING..."
+           " EXITING...\n");
+    exit(EXIT_FAILURE);
+  }
+
+  char *start, *end;
+  int size = 0;
+
+  if (mode == DATA) {
+    start = strchr(buffer, '>');
+    if (!start) {
+      perror("[ERROR] -> read_line: Could not locate the desired character in "
+             "read_line. EXITING...\n");
+      exit(EXIT_FAILURE);
+    }
+    // Want to point after >
+    start++;
+
+    // Pointer directly on the opening of the closing xml tag.
+    end = strchr(start, '<');
+    if (!end) {
+      perror("[ERROR] -> read_line: Could not locate the desired character in "
+             "read_line. EXITING...\n");
+      exit(EXIT_FAILURE);
+    }
+
+    size = end - start;
+
+    strncpy(extracted_string, start, size);
+    extracted_string[size] = '\0';
+    return extracted_string;
+  } else if (mode == TYPE) {
+    start = strchr(buffer, '<');
+    if (!start) {
+      perror("[ERROR] -> read_line: Could not locate the desired character in "
+             "read_line. EXITING...\n");
+      exit(EXIT_FAILURE);
+    }
+
+    // Want to point after >
+    start++;
+
+    // Pointer directly on the opening of the closing xml tag.
+    end = strchr(start, '>');
+    if (!end) {
+      perror("[ERROR] -> read_line: Could not locate the desired character in "
+             "read_line. EXITING...\n");
+      exit(EXIT_FAILURE);
+    }
+
+    size = end - start;
+
+    strncpy(extracted_string, start, size);
+    extracted_string[size] = '\0';
+
+    return extracted_string;
   }
 }
